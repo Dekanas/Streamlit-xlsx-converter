08b66ec238cbed21202f4984ed79525651f508fe
Dekanas <44368441+Dekanas@users.noreply.github.com>
2023-01-11 13:59:14 +0200

    Update xlsx_converter.py

--- xlsx_converter.py
+++ xlsx_converter.py
@@ -14,15 +14,16 @@ if uploaded_file is not None:
     # Transforming data
     df = df[['Reference No.', 'Claim No.', 'Country', 'Claim type', 'Became a claim?', 'Claim status (closed/open)', 'Claim open date', 'Claim close date', 'Total claim cost so far (EUR with VAT)', 'Repair cost (EUR with VAT)', 'Parts cost (with VAT)', 'Shipping paid by the service (EUR/with VAT)', 'Shiping cost (EUR with VAT)', 'Disposal cost (EUR with VAT)']]
 
-    # Create a function that returns the transformed data as an Excel file
-    def get_excel():
-        buffer = BytesIO()
-        export_file = df.to_excel(buffer, index = None, header=True)
-        buffer.seek(0)
-        return buffer
-
-    # Add a button that, when clicked, triggers a download of the transformed data
-    if st.button("Download transformed data"):
-        st.markdown("Transform Done!")
-        st.markdown("### Download transformed data:")
-        st.download_button("Download Excel File", get_excel, "transformed_data.xlsx")   
+    def convert_df(df):
+        return df.to_csv(index=False).encode('utf-8')
+    
+    csv = convert_df(df)
+    
+   st.download_button(
+       "Press to Download",
+       csv,
+       "file.csv",
+       "text/csv",
+       key='download-csv'
+    )
+    
