84558007f97537bff98fca905d71449514b139bf
Dekanas <44368441+Dekanas@users.noreply.github.com>
2023-01-11 13:50:15 +0200

    Update xlsx_converter.py

--- xlsx_converter.py
+++ xlsx_converter.py
@@ -3,19 +3,26 @@ import pandas as pd
 from io import BytesIO
 
 st.set_page_config(page_title="Excel Transformer", page_icon=":guardsman:", layout="wide")
-
 st.title("Excel Transformer")
 
-uploaded_file = st.file_uploader("Choose a CSV file", type=["xls", "xlsx"])
+uploaded_file = st.file_uploader("Choose an Excel file", type=["xls", "xlsx"])
 
 if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file, sheet_name = 'Claims')
+    df = pd.read_excel(uploaded_file)
     st.dataframe(df)
 
     # Transforming data
-    new_excel = df[['Reference No.', 'Claim No.', 'Country', 'Claim type', 'Became a claim?', 'Claim status (closed/open)', 'Claim open date', 'Claim close date', 'Total claim cost so far (EUR with VAT)', 'Repair cost (EUR with VAT)', 'Parts cost (with VAT)', 'Shipping paid by the service (EUR/with VAT)', 'Shiping cost (EUR with VAT)', 'Disposal cost (EUR with VAT)']]
-    st.dataframe(new_excel)
-    # Download transformed data
-    st.write("Transform Done!")
-    st.markdown("### Download transformed data:")
-    st.download_button('Download XLSX', new_excel, 'new_excel.xlsx', 'text/csv')
+     df = df[['Reference No.', 'Claim No.', 'Country', 'Claim type', 'Became a claim?', 'Claim status (closed/open)', 'Claim open date', 'Claim close date', 'Total claim cost so far (EUR with VAT)', 'Repair cost (EUR with VAT)', 'Parts cost (with VAT)', 'Shipping paid by the service (EUR/with VAT)', 'Shiping cost (EUR with VAT)', 'Disposal cost (EUR with VAT)']]
+
+    # Create a function that returns the transformed data as an Excel file
+    def get_excel():
+        buffer = BytesIO()
+        export_file = df.to_excel(buffer, index = None, header=True)
+        buffer.seek(0)
+        return buffer.getvalue()
+
+    # Add a button that, when clicked, triggers a download of the transformed data
+    if st.button("Download transformed data"):
+        st.markdown("Transform Done!")
+        st.markdown("### Download transformed data:")
+        st.download_button("Download Excel File", get_excel, "transformed_data.xlsx")   
