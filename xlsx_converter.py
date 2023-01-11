c70930abe4d45ddbe511fbf130bdceafa5f704be
Dekanas <44368441+Dekanas@users.noreply.github.com>
2023-01-11 14:25:20 +0200

    Update xlsx_converter.py

--- xlsx_converter.py
+++ xlsx_converter.py
@@ -17,12 +17,13 @@ if uploaded_file is not None:
     # Transforming data
     df = df[['Reference No.', 'Claim No.', 'Country', 'Claim type', 'Became a claim?', 'Claim status (closed/open)', 'Claim open date', 'Claim close date', 'Total claim cost so far (EUR with VAT)', 'Repair cost (EUR with VAT)', 'Parts cost (with VAT)', 'Shipping paid by the service (EUR/with VAT)', 'Shiping cost (EUR with VAT)', 'Disposal cost (EUR with VAT)']]
 
-    workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-    worksheet = workbook.add_worksheet()
-    for row_num, row_data in enumerate(df):
-        for col_num, col_data in enumerate(row_data):
-            worksheet.write(row_num, col_num, col_data)
-    workbook.close()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    worksheet.set_column('A:A', None, format1)
+    writer.save()
+    
     st.download_button(
         label="Download Excel workbook",
         data=output.getvalue(),
