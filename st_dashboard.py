224ad82a937cea6c9c58e9187976e81a2bea9151
Dekanas <44368441+Dekanas@users.noreply.github.com>
2023-01-13 10:22:16 +0200

    Add files via upload

---
+++ st_dashboard.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import streamlit_elements as st_elements
+from streamlit_elements import data
+
+st.set_page_config(page_title="My Dashboard", page_icon=":guardsman:", layout="wide", initial_sidebar_state="auto")
+
+st.title("My Dashboard")
+
+# Use the Drag and Drop component to create a draggable element
+draggable_element = st_elements.drag_and_drop.dnd_container(
+    children=[
+        st_elements.text_area(label="Enter some code", key="code")
+    ],
+    key="draggable_element",
+    style={'background': '#f2f2f2', 'padding': '20px'}
+)
+
+# Use the Resizable component to create a resizable element
+resizable_element = st_elements.resizable.resizable_container(
+    children=[
+        st_elements.select_box(label="Select a data set", options=["Data Set 1", "Data Set 2", "Data Set 3"], key="data_set")
+    ],
+    key="resizable_element",
+    style={'background': '#f2f2f2', 'padding': '20px'}
+)
+
+# Use the Grid component to create a grid layout
+grid_layout = st_elements.grid.grid_container(
+    children=[
+        resizable_element,
+        draggable_element
+    ],
+    key="grid_layout",
+    style={'background': '#f2f2f2', 'padding': '20px'}
+)
+
+st_elements.render_element(grid_layout)
+
+# Get the data from the draggable and resizable elements
+code = st_elements.get_state("code")
+data_set = st_elements.get_state("data_set")
+
+# Use the Chart component to create a chart
+chart_element = st_elements.chart.chart_container(
+    chart_type='bar',
+    data=data_set,
+    key="chart_element",
+    style={'background': '#f2f2f2', 'padding': '20px'}
+)
+st_elements.render_element(chart_element)
